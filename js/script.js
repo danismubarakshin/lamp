7d5015f376a273912ea77fa5a7f22f3654a11107
danismubarakshin <71936370+danismubarakshin@users.noreply.github.com>
2021-08-18 21:28:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('[name="country"]').on('change', function() {
+        var geoKey = $(this).find('option:selected').val();
+        var data = $jsonData.prices[geoKey];
+        var price = data.price;
+        var origPrice = price * 10;
+        var oldPrice = data.old_price;
+        var currency = data.currency
+        $("[value = "+geoKey+"]").attr("selected", true).siblings().attr('selected', false);
+
+        $('.sale .old').text(oldPrice + ' ' + currency);
+        $('.sale .new').text(price + ' ' + currency);
+    });
+});
+
+
+$(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+});
